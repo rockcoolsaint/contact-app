b33edc07808286a51ab403aba0f67b4ed0453be3
“innocent <rockcoolsaint@gmail.com>
2018-03-20 23:29:01 +0100

    update to settings

--- contacts/settings.py
+++ contacts/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+from decouple import config
+
 import os
 
 from decouple import config
