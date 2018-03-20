9c044842c07d58b222f4660fe612cb08c7d20a22
“innocent <rockcoolsaint@gmail.com>
2018-03-20 17:06:27 +0100

    statifile setting

--- contacts/settings.py
+++ contacts/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
