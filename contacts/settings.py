d1a48a080173997b676d0004aa115af7d036dc81
“innocent <rockcoolsaint@gmail.com>
2018-03-20 14:53:56 +0100

    deployment prep for heroku

--- contacts/settings.py
+++ contacts/settings.py
@@ -119,3 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_ages=500)
+DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())
